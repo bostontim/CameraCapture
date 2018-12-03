05ef876e7997dadf2bfc88fffa2c737d4ffefa9c
Tim <tim.gomeringer@gmail.com>
2018-12-02 22:00:14 -0500

    Add Exif tagging

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CameraCapture
+
+Dependencies
+
+OpenCV
+
+Exiv2
+-https://github.com/Exiv2/exiv2
+-hash: d8a15e1d132e260dd333ebc5af1e6929257a6d0f
+-environment: export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" 
+
--- src/capture_test.cpp
+++ src/capture_test.cpp
@@ -1,4 +1,5 @@
-#include<opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+#include <exiv2/exiv2.hpp>
 #include <iostream>
 #include <thread>
 #include <string>
@@ -12,6 +13,10 @@ string type;
 string in;
 VideoCapture usbcam;
 Mat frame;
+bool notDone = true;
+unsigned int itemCount = 0;
+string frontFile = "front.jpg";
+string backFile = "back.jpg";
 
 /* For future use.  Need to transfer thread handler for Qt
 void spinVideo() {
@@ -21,79 +26,137 @@ void spinVideo() {
 void getUserInput() 
 {
 
-   while(true)
+   while(notDone)
    {
 
-      printf("Position front side and press enter: ");
-      getline(cin, in);
+      itemCount++;
+      frontFile = "front" + to_string(itemCount) + ".jpeg";
+      backFile = "back" + to_string(itemCount) + ".jpeg";
 
-      if (in.empty())
+      while(true)
       {
 
-         printf("Capturing front image\n");
-         imwrite("front.jpg", frame);
-         break;
+         printf("Item %d: Position front side and press enter: ",itemCount);
+         getline(cin, in);
 
-      }
-      if (in == "c")
-      {
+         if (in.empty())
+         {
 
-         printf("Exiting user input thread\n");
-         return;
+            printf("Capturing front image\n");
+            imwrite(frontFile, frame);
+            break;
 
-      }
+         }
+         if (in == "c")
+         {
 
-   }
+            printf("Exiting user input thread\n");
+            notDone = false;
+            return;
 
-   while(true)
-   {
-
-      printf("Position back side and press enter: ");
-      getline(cin, in);
-
-      if (in.empty())
-      {
-
-         printf("Capturing back image\n");
-         imwrite("back.jpg", frame);
-         break;
-
-      }
-      if (in == "c")
-      {
-
-         printf("Exiting user input thread\n");
-         return;
+         }
 
       }
 
-   }
+      while(true)
+      {
 
-   while(true)
-   {
+         printf("Item %d: Position back side and press enter: ",itemCount);
+         getline(cin, in);
+
+         if (in.empty())
+         {
+
+            printf("Capturing back image\n");
+            imwrite(backFile, frame);
+            break;
+
+         }
+         if (in == "c")
+         {
+
+            printf("Exiting user input thread\n");
+            notDone = false;
+            return;
+
+         }
+
+      }
+
+      while(true)
+      {
+
+         // Use generic categories for now
+         printf("Item %d: Enter category (t, j, s): ",itemCount);
+         getline(cin, in);
+
+         if (in == "t" || in == "j" || in == "s")
+         {
+
+            type = in;
+            printf("Type is %s\n",type.c_str());
+            break;
+
+         }
+         if ( in == "c" )
+         {
+
+            printf("Exiting user input thread\n");
+            notDone = false;
+            return;
+
+         }
+   
+      }
+
+      // Exif tagging
+      printf("Tagging images\n");
+      Exiv2::Image::AutoPtr front_image = Exiv2::ImageFactory::open(frontFile);
+      Exiv2::ExifData ed_front;
+      ed_front["Exif.Image.ImageDescription"] = "Front";
+      // Use standard exif tag to store the item count for now
+      // Neet to create a custom MakerNote
+      ed_front["Exif.Image.Make"] = to_string(itemCount);
+      // Use standard exif tag to store the item type for now
+      // Neet to create a custom MakerNote
+      ed_front["Exif.Image.Model"] = type;
+      front_image->setExifData(ed_front);
+      front_image->writeMetadata();
+
+      Exiv2::Image::AutoPtr back_image = Exiv2::ImageFactory::open(backFile);
+      Exiv2::ExifData ed_back;
+      ed_back["Exif.Image.ImageDescription"] = "Back";
+      // Use standard exif tag to store the item count for now
+      // Neet to create a custom MakerNote
+      ed_back["Exif.Image.Make"] = to_string(itemCount);
+      // Use standard exif tag to store the item count for now
+      // Neet to create a custom MakerNote
+      ed_back["Exif.Image.Model"] = type;
+      back_image->setExifData(ed_back);
+      back_image->writeMetadata();
 
       // Use generic categories for now
-      printf("Enter category (t, j, s): ");
+      printf("Continue to next item (y/n)? ");
       getline(cin, in);
 
-      if (in == "t" || in == "j" || in == "s")
+      if (in == "y")
       {
 
-         type = in;
-         printf("Type is %s\n",type.c_str());
-         printf("Enter c in the camera window to close program.\n");
-         return;
+         // Continue
 
       }
-      if ( in == "c" )
+      else
       {
 
          printf("Exiting user input thread\n");
-         return;
+         printf("Enter c in the camera window to close program.\n");
+         notDone = false;
 
       }
-   
+
    }
+
+   return;
 
 }
 
